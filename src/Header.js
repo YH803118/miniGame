67aa038d8b37a0ca6b98816cdcb2ed8730bb0751
YH803118 <dudghks1045@naver.com>
2022-09-23 20:27:43 +0900

    layout set

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './style/Header.css';
+
+
+function Header() {
+    return (
+        <div className='Header'>
+            <h2 className='gameName'>오셀로</h2>
+        </div>
+    );
+}
+export default Header;
