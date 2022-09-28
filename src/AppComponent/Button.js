2568e8cf0362ca8ef2a7b2a3513d861fda427285
YH803118 <dudghks1045@naver.com>
2022-09-28 17:08:11 +0900

    5일차 - 가장자리에 둘수없는 문제 해결, 코드 간소화

--- src/AppComponent/Button.js
+++ src/AppComponent/Button.js
@@ -8,13 +8,14 @@ function Button({color, id,onClick, turn}) {
         //    onClick(e);
             if(!check(e, turn)){
                 alert('그곳엔 둘 수 없습니다.');
-            }
+            } else onClick(e);
         } else {
             alert('이미 고른 칸은 다시 고를 수 없습니다.');
         }
     }
 
     const check = (e, turn) => {
+        console.log('----------------------');
         const selectNum =  Number(e.target.id);
         const turnArr = [];
         if(turn.includes('my')){
@@ -25,107 +26,78 @@ function Button({color, id,onClick, turn}) {
             turnArr.push('mySelect');
         }
         let possible = false;
-
-        if(document.getElementById(selectNum+1).className.includes(turnArr[1])){
-            for(let i=selectNum+2; i<=Math.ceil(selectNum/8)*8;i++)
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum+1; i<=Math.ceil(selectNum/8)*8-1;i++){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                        document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                    }
-                        onClick(e);
-                        possible = true;
-                    }
+        const changeUp = (i, add) => {
+            let possible = false;
+            console.log('+'+add+' '+i);
+            if(document.getElementById(i).className.includes(turnArr[0])){
+                for(let j=selectNum+add; j<i;){
+                    if(document.getElementById(j).className.includes(turnArr[0])) break;
+                    document.getElementById(j).classList.replace(turnArr[1], turnArr[0]);
+                    j += add;
+                }
+                possible=true;
+                }
+            return possible;
+        }
+        const changeDown = (i, rem) => {
+            let possible = false;
+            console.log('-'+rem+' '+i);
+            if(document.getElementById(i).className.includes(turnArr[0])){
+                for(let j=selectNum-rem; j>i;){
+                    if(document.getElementById(j).className.includes(turnArr[0])) break;
+                    document.getElementById(j).classList.replace(turnArr[1], turnArr[0]);
+                    j -= rem;
+                }
+                possible=true;
+                }
+            return possible;
+        }
+        if(selectNum%8 !== 0 && document.getElementById(selectNum+1).className.includes(turnArr[1])){
+            for(let i=selectNum+2; i<=Math.ceil(selectNum/8)*8;i++){
+                if(changeUp(i, 1)){ possible = true; break;}
+            }
         }// 좌
-        if(document.getElementById(selectNum-1).className.includes(turnArr[1])){
-            for(let i=selectNum-2; i>=Math.floor(selectNum/8)*8+1;i--)
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum-1; i>=Math.floor(selectNum/8)*8+1;i--){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                        document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                    }
-                        onClick(e);
-                        possible = true;
-                    }
+        if(selectNum%8 !== 1 && document.getElementById(selectNum-1).className.includes(turnArr[1])){
+            for(let i=selectNum-2; i>=Math.floor((selectNum-1)/8)*8+1;i--){
+                if(changeDown(i, 1)){ possible = true; break;}
+            }
         }// 우
-        if(document.getElementById(selectNum-8).className.includes(turnArr[1])){
+        if(selectNum>8 && document.getElementById(selectNum-8).className.includes(turnArr[1])){
             for(let i=selectNum-8; i>=0;){
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum-8; i>=0;){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                        document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                        i -= 8;
-                    }
-                        onClick(e);
-                        possible = true;
-                    } i -= 8;
+                if(changeDown(i, 8)){ possible = true; break;}
+                i -= 8;
                 }
         }// 상
-        if(document.getElementById(selectNum+8).className.includes(turnArr[1])){
+        if(selectNum<57 && document.getElementById(selectNum+8).className.includes(turnArr[1])){
             for(let i=selectNum+8; i<=64;){
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum+8; i<=64;){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                        document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                        i += 8;
-                    }
-                        onClick(e);
-                        possible = true;
-                    } i += 8;
+                if(changeUp(i, 8)) { possible = true; break;}
+                i += 8;
                 }
         }// 하
-        if(document.getElementById(selectNum-7).className.includes(turnArr[1])){
-            for(let i=selectNum-7; i>=0;){
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum-7; i>=0;){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                        document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                        i -= 7;
-                    }
-                        onClick(e);
-                        possible = true;
-                    } i -= 7;
+        if(selectNum>9 && selectNum%8 !== 0 && document.getElementById(selectNum-7).className.includes(turnArr[1])){
+            for(let i=selectNum-7; i>0;){
+                if(changeDown(i,7)) { possible = true; break;}
+                 i -= 7;
                 }
-        }// 대각 - 우상
-        if(document.getElementById(selectNum-9).className.includes(turnArr[1])){
-            for(let i=selectNum-9; i>=0;){
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum-9; i>=0;){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                        document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                        i -= 9;
-                    }
-                        onClick(e);
-                        possible = true;
-                    } i -= 9;
+        }// 대각 - 좌하->우상
+        if(selectNum>9 && selectNum%8 !== 1 && document.getElementById(selectNum-9).className.includes(turnArr[1])){
+            for(let i=selectNum-9; i>0;){
+                if(changeDown(i, 9)) { possible = true; break;}
+                 i -= 9;
                 }
-        }// 대각 - 좌상
-        if(document.getElementById(selectNum+9).className.includes(turnArr[1])){
+        }// 대각 - 우하->좌상
+        if(selectNum<57 && selectNum%8 !== 0 && document.getElementById(selectNum+9).className.includes(turnArr[1])){
             for(let i=selectNum+9; i<=64;){
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum+9; i<=64;){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                            document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                            i += 9;
-                        }
-                        onClick(e);
-                        possible = true;
-                        } i += 9;
+                if(changeUp(i, 9)){ possible = true; break;}
+                 i += 9;
                     }
-        }// 대각 - 우하
-        if(document.getElementById(selectNum+7).className.includes(turnArr[1])){
+        }// 대각 - 좌상->우하
+        if(selectNum<57 && selectNum%8 !== 1 && document.getElementById(selectNum+7).className.includes(turnArr[1])){
             for(let i=selectNum+7; i<=64;){
-                if(document.getElementById(i).className.includes(turnArr[0])){
-                    for(let i=selectNum+7; i<=64;){
-                        if(document.getElementById(i).className.includes(turnArr[0])) break;
-                            document.getElementById(i).classList.replace(turnArr[1], turnArr[0]);
-                            i += 7;
-                        }
-                        onClick(e);
-                        possible = true;
-                        } i += 7;
+                if(changeUp(i, 7)) { possible = true; break;}
+                 i += 7;
                     }
-        }// 대각 - 좌하  
+        }// 대각 - 우상->좌하  
         return possible;
     }
 
